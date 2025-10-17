14e73d14dda4857eddd0cb7f573622664814110b
dylan-wolf <dylanalbertwolf@gmail.com>
2025-10-16 20:35:26 -0400

    Small updates

--- src/l0/l0_reader.py
+++ src/l0/l0_reader.py
@@ -5,3 +5,22 @@ path = r"/home/dylan-wolf/Projects/NASA/MAGNETAR_pipeline/data/SOFIA_HAWC_PLUS_F
 
 hdul = fits.open(path, memmap=True)
 print([hdu.name for hdu in hdul])
+
+cfg = hdul["Configuration"].data
+ts = hdul['Timestream'].data
+
+sq1 = ts["SQ1Feedback"]
+nsamp = sq1.shape[0]
+
+# --- Split into subarrays ---
+R0, R1, T0, T1 = (
+    sq1[:, :, 0:32],
+    sq1[:, :, 32:64],
+    sq1[:, :, 64:96],
+    sq1[:, :, 96:128],
+)
+
+print(R1)
+print(R0)
+print(T0)
+print(T1)
